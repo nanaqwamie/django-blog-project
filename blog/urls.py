eaa652e18ab0cf5ba9d578515c0766c9e76de5be
Aiti Student <aiti-student@aiti-091-PC.(none)>
2012-07-09 13:18:55 -0400

    django_blog_project1_views_urls

---
+++ blog/urls.py
@@ -0,0 +1,15 @@
+"""
+This code should be copy and pasted into blog/urls.py
+"""
+
+
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    url(r'^$', 'blog.views.home'),
+    url(r'^posts/$', 'blog.views.post_list'),
+    url(r'^posts/(?P<id>\d+)/((?P<showComments>.*)/)?$', 'blog.views.post_detail'),
+    ## add your url here
+
+    #url(r'^posts/search/(\w+)', include(blog.views.post_search))
+)
--- myblog/urls.py
+++ myblog/urls.py
@@ -13,4 +13,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^blog/', include('blog.urls')),	
+
 )
